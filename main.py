a1177bcc34dea0aa245687822f717add90aa6142
Suhatt Jantorn <graymoka@Suhatts-MacBook-Air.local>
2020-02-15 00:12:12 -0800

    day 1 lesson: interger/floast/character work around with function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-
+from math import * #--> give us more of a math function
 
 # excercise1 : variables / getting input
 
 character_name = "Jason Dern"
-character_age = "35" #--> will be back when to convert int to string
+character_age = 35
 is_male = True
 
 print("There once was a man name " + character_name)
-print("he was " + character_age + "years old")
+print("he was " + str(character_age) + " years old")
 character_name = "Mike Chance"
 print("he really liked the name " + character_name)
-print("but he didn't like the age " + character_age)
+print("but he didn't like the age " + str(character_age))
+print("-------------------------------------------------")
+
+
+print("-------------------------------------------------")
 
 phrase = "Giraffe Academy"
 print(phrase.lower())
@@ -19,3 +23,12 @@ print(len(phrase))
 print(phrase[0])
 print(phrase.index("Acad"))
 print(phrase.replace("Giraffe","Elepheant"))
+
+# excercise2 : function -> is a collection of code that does someting
+
+print(abs(-5))
+print(pow(4,5))
+print(max(325,144)) #as wll as min()
+print(round(4.5))
+
+print(sqrt(75) + floor(3.4) + ceil(8.6)) #--> get math function
