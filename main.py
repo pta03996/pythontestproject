88a31f45989e95343beb082ee3e1029e514a5fb5
Suhatt Jantorn <graymoka@Suhatts-MacBook-Air.local>
2020-02-14 17:56:55 -0800

    learn variable

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello github")
+
+
+# excercise1 : variables / getting input
+
+character_name = "Jason Dern"
+character_age = "35" #--> will be back when to convert int to string
+is_male = True
+
+print("There once was a man name " + character_name)
+print("he was " + character_age + "years old")
+character_name = "Mike Chance"
+print("he really liked the name " + character_name)
+print("but he didn't like the age " + character_age)
+
+phrase = "Giraffe Academy"
+print(phrase.lower())
+print(phrase.lower().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe","Elepheant"))
