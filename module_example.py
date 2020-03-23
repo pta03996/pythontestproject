d73bb857dc776ab64d01022bc146e8def6d695fe
graymoka <heaven.cuervo@gmail.com>
2020-03-23 11:03:30 -0700

    call out new module

--- module_example.py
+++ module_example.py
@@ -1,6 +1,11 @@
-import util
+import utils
 
-print(util.roll_dice(6))
+print(utils.roll_dice(6))
+
+
+numbers = [10,2,5,7]
+maximum_number = utils.find_max_number(numbers)
+print(maximum_number)
 
 # how to install external module
 #    pip install python-docx
